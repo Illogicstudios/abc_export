7dd35a2c1acc8fa94c783cefe77bd59de21891b9
mariusjenin <marius2401@laposte.net>
2023-03-07 14:25:03 +0100

    clean the scene before export

--- ABCExportAsset.py
+++ ABCExportAsset.py
@@ -10,6 +10,84 @@ except:
 
 class ABCExportAsset:
 
+    @staticmethod
+    def __optimize_scene_size():
+        print("\nvvvvvvvvvvvvvvvv Optimize Scene Size vvvvvvvvvvvvvvvv")
+        mel.source('cleanUpScene')
+        mel.scOpt_performOneCleanup({
+            "nurbsSrfOption",
+            "setsOption",
+            "transformOption",
+            "renderLayerOption",
+            "renderLayerOption",
+            "animationCurveOption",
+            "groupIDnOption",
+            "unusedSkinInfsOption",
+            "groupIDnOption",
+            "shaderOption",
+            "ptConOption",
+            "pbOption",
+            "snapshotOption",
+            "unitConversionOption",
+            "referencedOption",
+            "brushOption",
+            "unknownNodesOption",
+        })
+        print("^^^^^^^^^^^^^^^^ Optimize Scene Size ^^^^^^^^^^^^^^^\n")
+
+    @staticmethod
+    def __delete_unknown_node():
+        print("\nvvvvvvvvvvvvvvv Delete Unknown Nodes vvvvvvvvvvvvvvvv")
+        unknown = ls(type="unknown")
+        if unknown:
+            print("Removing:" + unknown)
+            delete(unknown)
+        else:
+            print("No unknown nodes found")
+        print("^^^^^^^^^^^^^^^ Delete Unknown Nodes ^^^^^^^^^^^^^^^^\n")
+
+    @staticmethod
+    def __remove_unknown_plugins():
+        print("\nvvvvvvvvvvvvvv Remove Unknown Plugins vvvvvvvvvvvvvvv")
+        old_plug = cmds.unknownPlugin(query=True, list=True)
+        if old_plug:
+            for plug in old_plug:
+                print("Removing:" + plug)
+                try:
+                    cmds.unknownPlugin(plug, remove=True)
+                except Exception as e:
+                    print(e)
+        else:
+            print("No unknown plugin found")
+        print("^^^^^^^^^^^^^^ Remove Unknown Plugins ^^^^^^^^^^^^^^^\n")
+
+    @staticmethod
+    def __unlock_all_nodes():
+        print("\n------------------ Unlock All Nodes -----------------\n")
+        all_nodes = ls()
+        if all_nodes:
+            for node in all_nodes:
+                lockNode(node, l=False)
+
+    @staticmethod
+    def __remove_blast_panel_error():
+        print("\n------------ Remove CgAbBlastPanel Error ------------\n")
+        for model_panel in getPanel(typ="modelPanel"):
+            # Get callback of the model editor
+            callback = modelEditor(model_panel, query=True, editorChanged=True)
+            # If the callback is the erroneous `CgAbBlastPanelOptChangeCallback`
+            if callback == "CgAbBlastPanelOptChangeCallback":
+                # Remove the callbacks from the editor
+                modelEditor(model_panel, edit=True, editorChanged="")
+        if objExists("uiConfigurationScriptNode"):
+            delete("uiConfigurationScriptNode")
+
+    @staticmethod
+    def __fix_isg():
+        print("\n-------------- Fix initialShadingGroup --------------\n")
+        lockNode('initialShadingGroup', lock=0, lockUnpublished=0)
+        lockNode('initialParticleSE', lock=0, lockUnpublished=0)
+
     # Get the next version (version when the ABC will be exported)
     @staticmethod
     def next_version(folder):
@@ -52,7 +130,14 @@ class ABCExportAsset:
         return self.__geos
 
     # Export this reference in ABC in a new version
-    def export(self, folder, start, end, subsamples_enbaled, subsamples, euler_filter):
+    def export(self, folder, start, end, subsamples_enabled, subsamples, euler_filter):
+        self.__optimize_scene_size()
+        self.__delete_unknown_node()
+        self.__remove_unknown_plugins()
+        self.__unlock_all_nodes()
+        self.__remove_blast_panel_error()
+        self.__fix_isg()
+
         abc_name = self.get_name_with_num()
         asset_dir_path = os.path.join(folder, abc_name)
         next_version = ABCExportAsset.next_version(asset_dir_path)
@@ -63,7 +148,7 @@ class ABCExportAsset:
         os.makedirs(version_dir_path, exist_ok=True)
 
         command = "-frameRange %s %s" % (start, end)
-        if subsamples_enbaled:
+        if subsamples_enabled:
             command +=" -step 1.0"
             for frame in subsamples.split(" "):
                 command += " -frameRelativeSample " + frame
